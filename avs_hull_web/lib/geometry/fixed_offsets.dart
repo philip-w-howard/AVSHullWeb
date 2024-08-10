b8434baeda283ad1ad948302ff9de927547bca7c
Phil Howard <phil.w.howard@gmail.com>
2024-08-10 11:44:17 -0700

    fixed first/last issue in fixed offsset output

--- avs_hull_web/lib/geometry/fixed_offsets.dart
+++ avs_hull_web/lib/geometry/fixed_offsets.dart
@@ -17,36 +17,33 @@ List<Offset> getFixedOffsets(Panel source, double fixedOffset)
 
   Offset p1 = sourceOffsets[sourceOffsets.length - 2];
   Offset p2 = sourceOffsets[sourceOffsets.length - 1];
-  Offset p3;
+  Offset p3 = p1;
+
   bool first = true;
 
   for (Offset p in sourceOffsets)
   {
     p3 = p;
 
-    if (first)
+    if (!first && isKnee(p1, p2, p3, _kneeAngle))
     {
-      print('add first ${p3.toString()}');
-      offsets.add(p3);
-      first = false;
-    }
-    else if (isKnee(p1, p2, p3, _kneeAngle))
-    {
-      print('add knee ${p2.toString()}');
       offsets.add(p2);
     }
+
+    first = false;
 
 
     if (spansX(p2, p3, fixedOffset.toInt()))
     {
       Offset temp = computeSpacingPoint(p2, p3, fixedOffset.toInt());
-      print('add span ${temp.toString()}');
       offsets.add(temp);
     }
 
     p1 = p2;
     p2 = p3;
   }
+
+  offsets.add(p3);
 
   return offsets;
 }
