37d7a53779ad4a39a1040e38b26bc7908161af17
Phil Howard <phil.w.howard@gmail.com>
2025-08-28 13:43:45 -0700

    Updated version to 5.2

--- avs_hull_web/lib/settings/settings.dart
+++ avs_hull_web/lib/settings/settings.dart
@@ -20,7 +20,7 @@ const hullParamsKey = 'HullParams';
 const exportOffsetsParamsKey = 'ExportOffsetsParams';
 const waterlineParamsKey = 'WaterlineParams';
 const unnamedHullName = 'unnamed';
-const version = '0.5.1';
+const version = '0.5.2';
 
 // ***********************************************************
 // ***********************************************************
